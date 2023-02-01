0627db5e04b6e7872b28a82bc89e5acf52b8b891
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-02-01 17:14:22 +0100

    Initial commit

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ import Grid from "./Grid.js"
 const gameBoard = document.getElementById("game-board")
 
 const grid = new Grid(gameBoard)
-grid.randomEmptyCell().title = new Title(gameBoard)
-grid.randomEmptyCell().title = new Title(gameBoard)
+// console.log(grid.randomEmptyCell())
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
